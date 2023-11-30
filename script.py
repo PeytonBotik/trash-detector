d1fc5293c2f5c4ac1fa307b3b63360d32b0514b1
PeytonBotik <peytonbotik@gmail.com>
2023-11-29 20:07:14 -0600

    Adds simple input for debugging.

--- script.py
+++ script.py
@@ -9,11 +9,19 @@ interpreter.allocate_tensors()
 input_details = interpreter.get_input_details()
 output_details = interpreter.get_output_details()
 
+# Example expected shape: [1, 224, 224, 3]
+height = 224
+width = 224
+channels = 3
+
+# Create an array of zeros or ones
+simple_input = np.ones((1, height, width, channels), dtype=np.float32)
+
 # Prepare your input data. (This depends on your model)
 input_data = np.array(np.random.random_sample(input_details[0]['shape']), dtype=np.float32)
 
 # Set the tensor (input data)
-interpreter.set_tensor(input_details[0]['index'], input_data)
+interpreter.set_tensor(input_details[0]['index'], simple_input)
 
 # Run inference
 interpreter.invoke()
